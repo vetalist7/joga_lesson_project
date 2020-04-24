656ab7d2a0ca083fb8f274260a5b63905fc41fd1
Vitalii Bakymenko <vetalist7@ukr.net>
2020-04-24 23:44:28 +0300

    2 commit
    Add timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,58 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Timer
+    // parse - зводить до мілісекнд
+
+    let deadline = '2020-04-30';
+    
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            // отримано остаток від ділення, це будудть секунди
+        
+        return {
+            'total' : t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '0';
+                minutes.textContent = '0';
+                seconds.textContent = '0';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
 });
